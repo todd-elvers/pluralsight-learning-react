0a9cb37eae89004337184744a0eb28525ca99698
Todd Elvers <todd.elvers@gmail.com>
2016-10-06 20:46:47 -0500

    Tried out some 'willTransitionTo/From' React Router methods

--- src/components/about/aboutPage.jsx
+++ src/components/about/aboutPage.jsx
@@ -3,6 +3,25 @@
 var React = require('react');
 
 var About = React.createClass({
+    statics: {
+        willTransitionTo: function (transition, params, query, callback) {
+            if(!confirm('Are you sure you want to read a page that\'s boring?')) {
+                // This prevents the transition from occurring, keeping the user on the same page
+                transition.abort();
+            } else {
+                // This triggers the transition to occur
+                callback();
+            }
+        },
+
+        willTransitionFrom: function (transition, component) {
+            if(!confirm('Are you sure you want to leave a page that\'s this exiting?')) {
+                // This prevents the transition from occurring, keeping the user on the same page
+                transition.abort();
+            }
+        }
+    },
+
     render: function() {
         return (
             <div>
