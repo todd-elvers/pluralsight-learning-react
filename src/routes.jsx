536a7f4694c4f62184529ddd236947d992fb1ab2
Todd Elvers <todd.elvers@gmail.com>
2016-10-06 20:27:34 -0500

    Implemented a 404 page in React Router

--- src/routes.jsx
+++ src/routes.jsx
@@ -5,12 +5,14 @@ var React = require('react');
 var Router = require('react-router');
 var DefaultRoute = Router.DefaultRoute;
 var Route = Router.Route;
+var NotFoundRoute = Router.NotFoundRoute;
 
 var routes = (
     <Route name="app" path="/" handler={require('./components/app.jsx')}>
         <DefaultRoute handler={require('./components/homePage.jsx')}/>
         <Route name="authors" handler={require('./components/authors/authorPage.jsx')}/>
         <Route name="about" handler={require('./components/about/aboutPage.jsx')}/>
+        <NotFoundRoute handler={require('./components/notFoundPage.jsx')} />
     </Route>
 );
 
