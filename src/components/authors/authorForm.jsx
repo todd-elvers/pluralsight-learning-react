880dda7cc3693161c20a21a606a4f9375b2f7a29
Todd Elvers <todd.elvers@gmail.com>
2016-10-08 16:34:43 -0500

    Wired up a form

---
+++ src/components/authors/authorForm.jsx
@@ -0,0 +1,27 @@
+"use strict";
+
+var React = require('react');
+var Input = require('../common/textInput.jsx');
+
+var AuthorForm = React.createClass({
+    render: function () {
+        return (
+            <form>
+                <h1>Manage Author</h1>
+                <Input name="firstName"
+                       label="First Name"
+                       value={this.props.author.firstName}
+                       onChange={this.props.onChange} />
+
+                <Input name="lastName"
+                       label="Last Name"
+                       value={this.props.author.lastName}
+                       onChange={this.props.onChange} />
+
+                <input type="submit" value="Save" className="btn btn-default" />
+            </form>
+        );
+    }
+});
+
+module.exports = AuthorForm;
--- src/routes.jsx
+++ src/routes.jsx
@@ -12,6 +12,7 @@ var routes = (
     <Route name="app" path="/" handler={require('./components/app.jsx')}>
         <DefaultRoute handler={require('./components/homePage.jsx')}/>
         <Route name="authors" handler={require('./components/authors/authorPage.jsx')}/>
+        <Route name="addAuthor" path="author" handler={require('./components/authors/manageAuthorPage.jsx')}/>
         <Route name="about" handler={require('./components/about/aboutPage.jsx')}/>
         <NotFoundRoute handler={require('./components/notFoundPage.jsx')} />
         <Redirect from="about-us" to="about" />
